7e30241afdbfc3483f47248e8fdea5ea6353f81b
chenggangpro <chenggangpro@gmail.com>
2026-01-30 11:57:28 +0800

    init project

---
+++ simple-multi-round-agent/src/main/java/pro/chenggang/project/agentplayground/simplemultiround/SimpleMultiRoundAgentApplication.java
@@ -0,0 +1,16 @@
+package pro.chenggang.project.agentplayground.simplemultiround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Cheng Gang
+ * @version 0.1.0
+ */
+@SpringBootApplication
+public class SimpleMultiRoundAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMultiRoundAgentApplication.class, args);
+    }
+}
---
+++ simple-multi-round-agent/src/test/java/pro/chenggang/project/agentplayground/simplemultiround/SimpleMultiRoundAgentApplicationTests.java
@@ -0,0 +1,11 @@
+package pro.chenggang.project.agentplayground.simplemultiround;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Cheng Gang
+ * @version 0.1.0
+ */
+@SpringBootTest
+public class SimpleMultiRoundAgentApplicationTests {
+}
